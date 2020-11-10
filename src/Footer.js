daf92adca8bcb0467f4dd407e7fd85517866f122
irfaul <muhirfaul@gmail.com>
2020-11-10 08:47:09 +0700

    adding file repo

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Footer.css'
+function Footer() {
+  return (
+    <div className="footer">
+      <p><strong>2020</strong>. <a href="https://www.instagram.com/iimmafahir/" target="_blank">iimmafahir</a>.</p>
+    </div>
+  )
+}
+
+export default Footer
